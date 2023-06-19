8d6e7aa8942da6e564e6c1786a4fbefd30610640
Stanislav <stasjazz10@gmail.com>
2023-06-19 11:45:08 +0100

    first edition

--- pwidder/urls.py
+++ pwidder/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('profile/<int:pk>', views.profile, name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
